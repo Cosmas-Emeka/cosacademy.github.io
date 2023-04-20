5a7c90398ce651ee1ccc67cb1898e02229ed87d8
Cosmas-Emeka <cosmasemeka11@gmail.com>
2023-04-20 14:49:56 +0100

    Created javacsript file for toggle menu

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var navLinks = document.getElementById("navLinks");
 var menuIcon = document.getElementsById("menuIcon");
+var head = document.getElementById("head");
+
 
 function showMenu() {
-    navLinks.style.right = "30%";
+    navLinks.style.right = "30vw";
 }
+
 function hideMenu() {
-    navLinks.style.right = "-50%";
+    navLinks.style.right = "-50vw";
+}
+
+function changeColor() {
+    head.style.background = "rgba(14, 9, 30, 0.7)"
 }
